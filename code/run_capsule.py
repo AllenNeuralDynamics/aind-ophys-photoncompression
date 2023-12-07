e57344e0e080fb7c459f9c108d7fd19aa9a0b739
Jerome Lecoq <jeromel@alleninstitute.org>
2023-12-07 20:16:47 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
     experiment_id = h5_file.name.split("_")[0].split(".")[0]
     print(experiment_id)
     platform_file = experiment_id + "_platform.json"
-    processing_json_fp = h5_file.parent / platform_file
+    processing_json_fp = h5_file.parent.parent / "processing.json"
     with open(processing_json_fp, "r") as j:
         data = json.load(j)
         
